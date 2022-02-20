bcbb5126a572af25750a4c48d744ea6e1eff8263
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-19 20:17:20 -0500

    Update print-interval.cpp

--- print-interval.cpp
+++ print-interval.cpp
@@ -1,1 +1,25 @@
+/*
+Author: Sakib Khan
+Course: CSCI-135
+Instructor: Tong Yi
+Assignment: Lab 2 B
+This program will print numbers between two intervals inputted.
+*/
+#include <iostream>
+using namespace std;
 
+int main()
+{
+    int L;
+    int U;
+    //Declares lower and upper bounds of interval
+    cout << "Input first number: ";
+    cin >> L;
+    cout << "Input second number: ";
+    cin >> U;
+    for(int i = L; i < U; i++){
+    //increment from first number to second number
+        cout << i;
+    }
+
+}
